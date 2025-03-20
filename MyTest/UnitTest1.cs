17d642a498da02244d9257fee00036e6b5a3f705
Vladimir420911 <dhbtgvhb@gmail.com>
2025-03-20 12:48:16 +0300

    Сделал тест для функции GenerateRevenueReport

--- MyTest/UnitTest1.cs
+++ MyTest/UnitTest1.cs
@@ -1,14 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLib;
 
 namespace MyTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        List<Appointment> appointments = new List<Appointment>
         {
+            new Appointment("Олег", "ноготки", 500, "Мастер1", new DateTime(2025, 01, 15)),
+            new Appointment("Олег2", "пушка калатушка", 750, "Мастер2", new DateTime(2025, 02, 15)),
+            new Appointment("Кельвин", "бомба петарррррда", 450, "Мастер3", new DateTime(2025, 03, 15))
+        };
+        [TestMethod]
+        public void TestGenerateRevenueReport()
+        {
+            DateTime startDate = new DateTime(2025, 02, 14);
+            DateTime endDate = new DateTime(2025, 03, 16);
+            string expOutput = $"Выручка за период с {startDate:d} по {endDate:d}: {1200:C}";
+
+            string result = GenerateRevenueReport(startDate, endDate);
+
+            Assert.AreEqual(expOutput, result);
         }
+
+        public string GenerateRevenueReport(DateTime startDate, DateTime endDate)
+        {
+            var revenue = appointments
+                .Where(a => a.Date >= startDate && a.Date <= endDate)
+                .Sum(a => a.Price);
+            return $"Выручка за период с {startDate:d} по {endDate:d}: {revenue:C}";
+        }
+
+
     }
 }
--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -64,9 +64,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 372);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(111, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Создать отчет";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
