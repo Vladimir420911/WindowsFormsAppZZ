c410ff9d552c4d18735c7de8192f19ad95340759
Vladimir420911 <dhbtgvhb@gmail.com>
2025-03-20 12:30:51 +0300

    В WinForms сделал создание отчета по доходу за определенный срок

---
+++ MyLib/Appointment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class Appointment
+    {
+        public string Client { get; set; }
+        public string Service { get; set; }
+        public int Price { get; set; }
+        public string Master { get; set; }
+        public DateTime Date { get; set; }
+
+        public Appointment(string client, string service, int price, string master, DateTime date)
+        {
+            Client = client;
+            Service = service;
+            Price = price;
+            Master = master;
+            Date = date;
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,33 @@ namespace WindowsFormsApp1
 {
     public partial class Form1: Form
     {
+        List<Appointment> appointments = new List<Appointment>
+
+        {
+            new Appointment("Олег", "ноготки", 500, "Мастер1", new DateTime(2025, 01, 15)),
+            new Appointment("Олег2", "пушка калатушка", 750, "Мастер2", new DateTime(2025, 02, 15)),
+            new Appointment("Кельвин", "бомба петарррррда", 450, "Мастер3", new DateTime(2025, 03, 15))
+        };
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = appointments;
+        }
+
+        public string GenerateRevenueReport(DateTime startDate, DateTime endDate)
+        {
+            var revenue = appointments
+                .Where(a => a.Date >= startDate && a.Date <= endDate)
+                .Sum(a => a.Price);
+            return textBox1.Text = $"Выручка за период с {startDate:d} по {endDate:d}: {revenue:C}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateRevenueReport(dateTimePicker1.Value, dateTimePicker2.Value);
+
         }
     }
+
+
 }
