b463bbec40777c2d4d8e06138196034ec2d687e1
Vladimir420911 <dhbtgvhb@gmail.com>
2025-03-20 12:56:54 +0300

    Перенес функцию из Form1 в класс Salon

---
+++ MyLib/Salon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class Salon
+    {
+        public string GenerateRevenueReport(DateTime startDate, DateTime endDate, List<Appointment> appointments)
+        {
+            var revenue = appointments
+                .Where(a => a.Date >= startDate && a.Date <= endDate)
+                .Sum(a => a.Price);
+            string output = $"Выручка за период с {startDate:d} по {endDate:d}: {revenue:C}";
+            return output;
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,8 +13,8 @@ namespace WindowsFormsApp1
 {
     public partial class Form1: Form
     {
+        Salon salon = new Salon();
         List<Appointment> appointments = new List<Appointment>
-
         {
             new Appointment("Олег", "ноготки", 500, "Мастер1", new DateTime(2025, 01, 15)),
             new Appointment("Олег2", "пушка калатушка", 750, "Мастер2", new DateTime(2025, 02, 15)),
@@ -25,19 +25,9 @@ namespace WindowsFormsApp1
             InitializeComponent();
             dataGridView1.DataSource = appointments;
         }
-
-        public string GenerateRevenueReport(DateTime startDate, DateTime endDate)
-        {
-            var revenue = appointments
-                .Where(a => a.Date >= startDate && a.Date <= endDate)
-                .Sum(a => a.Price);
-            return textBox1.Text = $"Выручка за период с {startDate:d} по {endDate:d}: {revenue:C}";
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            GenerateRevenueReport(dateTimePicker1.Value, dateTimePicker2.Value);
-
+            textBox1.Text = salon.GenerateRevenueReport(dateTimePicker1.Value, dateTimePicker2.Value, appointments);
         }
     }
 
